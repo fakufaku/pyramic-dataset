fabaa5c838d39d9eb62114921f79dd8ae7a71fbc
Robin Scheibler <fakufaku@gmail.com>
2018-03-29 00:03:16 +0900

    Code for the mass Wiener deconvolution of the impulse response from the sweep recordings.

--- code/deconvolution.py
+++ code/deconvolution.py
@@ -8,6 +8,7 @@ from __future__ import division, print_function
 
 import numpy as np
 from scipy import linalg as la
+from scipy.interpolate import interp1d
 
 try:
     import mkl_fft as fft
@@ -15,9 +16,48 @@ try:
 except ImportError:
     import numpy.fft as fft
 
-def deconvolve(y, s, thresh=0.05):
+def plot_spectrum(x, fs, *args, **kwargs):
+    n = x.shape[0]
+    f = np.linspace(0, 0.5, n // 2 + 1) * fs
+    X = np.abs(np.fft.rfft(x, axis=0)) / np.sqrt(n)
+    plt.plot(f, 20*np.log10(X), *args, **kwargs)
+
+def periodogram(data, p_len, n=None):
     '''
-    Deconvolve an excitation signal from an impulse response
+    Compute the Periodogram of the input data
+
+    Parameters
+    ----------
+    data: ndarray
+        The input data
+    p_len: int
+        The number of points in the analyisis periodogram
+    n: int, optional
+        Optionaly, the periodogram is linearly resampled to this number of point
+    '''
+
+    n_frames = (data.shape[0] // p_len)
+    if data.ndim == 1:
+        frames = data[:n_frames*p_len].reshape((n_frames,p_len))
+    elif data.ndim == 2:
+        frames = data[:n_frames*p_len].reshape((n_frames,p_len,-1))
+    else:
+        raise ValueError('Dimension larger than 2 not supported')
+
+    P = np.mean(np.abs(np.fft.rfft(frames, axis=1) / np.sqrt(p_len))**2, axis=0)
+
+    if n is not None:
+        f = np.arange(p_len // 2 + 1) / p_len
+        interpolator = interp1d(f, P, kind='linear', axis=0)
+        f_long = np.arange(n // 2 + 1) / n
+        P = interpolator(f_long)
+
+    return P
+
+
+def deconvolve(y, s, noise, periodogram_len=64):
+    '''
+    Wiener deconvolution of an excitation signal from an impulse response
 
     Parameters
     ----------
@@ -26,6 +66,11 @@ def deconvolve(y, s, thresh=0.05):
         The recording
     s : ndarray
         The excitation signal
+    noise: ndarray
+        The noise reference signal
+
+    Returns
+    -------
     h_len : int
         The length of the impulse response
     '''
@@ -37,17 +82,84 @@ def deconvolve(y, s, thresh=0.05):
     if n % 2 != 0:
         n += 1
 
-    # Forward transforms
-    Y  = fft.rfft(np.array(y, dtype=np.float32), n=n) / np.sqrt(n)
-    S = fft.rfft(np.array(s, dtype=np.float32), n=n) / np.sqrt(n)
+    # Compute the periodogram of the noise signal
+    N = periodogram(noise, periodogram_len, n=n)
 
-    # Only do the division where S is large enough
-    H = np.zeros(*Y.shape, dtype=Y.dtype)
-    I = np.where(np.abs(S) > thresh)
-    H[I] = Y[I] / S[I]
+    # Forward transforms
+    Y  = fft.rfft(y, n=n, axis=0) / np.sqrt(n)
+    S = fft.rfft(s, n=n, axis=0) / np.sqrt(n)
+
+    # Wiener deconvolution
+    if Y.ndim > 1:
+        S = S[:,None]
+
+    H = Y * np.conj(S) / (np.abs(S)**2 + N)
 
     # Inverse transform
-    h = fft.irfft(H, n=n)
+    h = fft.irfft(H, n=n, axis=0)
 
     return h
 
+if __name__ == '__main__':
+
+    import argparse
+    import matplotlib.pyplot as plt
+    from scipy.io import wavfile
+    import pyroomacoustics as pra
+
+    parser = argparse.ArgumentParser(description='Deconvolve an impulse response')
+    parser.add_argument('recording', type=str, help='The recording file to deconvolve')
+    parser.add_argument('signal', type=str, help='The known test signal used at recording time')
+    parser.add_argument('noise', type=str, help='A template of the noise signal')
+    parser.add_argument('-l', '--length', type=int, help='The number of samples to which we limit the length of impulse response')
+    args = parser.parse_args()
+
+
+    r_rec, data_rec = wavfile.read(args.recording)
+    r_test, data_test = wavfile.read(args.signal)
+    r_noise, data_noise = wavfile.read(args.noise)
+
+    pre_proc = lambda x: x.astype(np.float32) - np.mean(x, axis=0)
+    data_rec = pre_proc(data_rec)
+    data_test = pre_proc(data_test)
+    data_noise = pre_proc(data_noise)
+
+    if r_rec != r_test:
+        raise ValueError('The two signals need to have the same sampling frequency')
+
+    fs = r_rec
+
+    h = deconvolve(data_rec, data_test, data_noise, periodogram_len=64)
+
+    if args.length is not None:
+        h = h[:args.length,:]
+
+    plt.figure()
+
+    plt.subplot(2,2,1)
+    plot_spectrum(data_rec, fs)
+    plt.title('Recording')
+
+    plt.subplot(2,2,2)
+    plot_spectrum(data_test, fs)
+    plt.title('Test Signal')
+
+    plt.subplot(2,2,3)
+    Pn = periodogram(data_noise, 64)
+    f = np.linspace(0, 0.5, len(Pn)) * fs
+    plot_spectrum(data_noise, fs, 'g')
+    plt.plot(f, 10*np.log10(Pn), 'r')
+    plt.title('Noise')
+
+    plt.subplot(2,2,4)
+    plot_spectrum(h, fs)
+    plt.title('Deconvolved Signal')
+
+    plt.tight_layout(pad=0.1)
+
+    plt.figure()
+    plt.plot(h)
+
+    plt.tight_layout(pad=0.1)
+
+    plt.show()
---
+++ code/run_deconvolution.py
@@ -0,0 +1,102 @@
+import sys, argparse, os
+from scipy.io import wavfile
+import numpy as np
+
+sys.path.append('./code')
+from deconvolution import deconvolve
+
+angles = np.arange(0, 360, 2)
+spkrs = [0, 1, 2]
+
+filename_fmt = '{sweep}/{sweep}_spkr{spkr}_angle{angle}.wav'
+noise_ref_fmt = 'silence/silence_spkr{spkr}_angle{angle}.wav'
+
+def pre_proc(x):
+    ''' Convert to floating points and remove the DC component '''
+    return x.astype(np.float32) - np.mean(x, axis=0)
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description=(
+          'Deconvolve the response of the array for all' 
+        + 'of the recordings and stores the impulse responses')
+        )
+    parser.add_argument('test_signal', type=str, help='The test signal used for the recordings')
+    parser.add_argument('segmented_dir', type=str, help='Location of segmented recordings')
+    parser.add_argument('output_dir', type=str, help='Location where to store the impulse responses')
+    parser.add_argument('-q', '--qc', action='store_true', help='Create plots for quality control')
+    parser.add_argument('-l', '--length', type=int, help='cut-off length of the impulse response')
+    parser.add_argument('-f', '--force', action='store_true', help='Overwrite output file if existing')
+    args = parser.parse_args()
+
+    r_sweep, data_sweep = wavfile.read(args.test_signal)
+
+    sweep_name = os.path.basename(args.test_signal)
+    sweep_name = os.path.splitext(sweep_name)[0]
+
+    if not os.path.exists(args.output_dir):
+        os.mkdir(args.output_dir)
+
+    if args.qc:
+        import matplotlib.pyplot as plt
+        qc_dir = os.path.join(args.output_dir, 'qc')
+        if not os.path.exists(qc_dir):
+            os.mkdir(qc_dir)
+
+    for spkr in spkrs:
+        for angle in angles:
+
+            # output file name
+            bname = 'ir_spkr{}_angle{}.wav'.format(spkr, angle)
+            fn_out = os.path.join(args.output_dir, bname)
+
+            if os.path.exists(fn_out):
+                print('spkr={} angle={}: file exists. Skip'.format(spkr, angle))
+                continue
+
+            # open file with noise reference
+            bname = noise_ref_fmt.format(spkr=spkr, angle=angle)
+            fn_noise = os.path.join(args.segmented_dir, bname)
+            r_noise, data_noise = wavfile.read(fn_noise)
+            data_noise = pre_proc(data_noise)
+
+            # Resample noise if necessary
+            if r_noise != r_sweep:
+                from samplerate import resample
+                data_noise = resample(data_noise, r_sweep / r_noise, 'sinc_best')
+
+            # open recording file
+            bname = filename_fmt.format(sweep=sweep_name, spkr=spkr, angle=angle)
+            fn_rec = os.path.join(args.segmented_dir, bname)
+            r_rec, data_rec = wavfile.read(fn_rec)
+            data_rec = pre_proc(data_rec)
+
+            if r_rec != r_sweep:
+                raise ValueError('Some mismatch in the sampling frequencies')
+
+            ir = deconvolve(data_rec, data_sweep, data_noise, periodogram_len=64)
+
+            if args.length is not None:
+                ir = ir[:args.length,:]
+
+            wavfile.write(fn_out, r_rec, ir)
+
+            if args.qc:
+                time = np.arange(ir.shape[0]) / r_rec * 1000
+                plt.plot(time, ir)
+                plt.xlabel('time [ms]')
+                plt.title('IR speaker {} angle {}'.format(spkr, angle))
+                plt.tight_layout(pad=0.1)
+                bname = 'ir_spkr{}_angle{}.png'.format(spkr, angle)
+                fn_out = os.path.join(qc_dir, bname)
+                plt.savefig(fn_out)
+
+            print('Speaker', spkr, 'Angle', angle, '... done.')
+
+
+            
+
+
+
+
+
