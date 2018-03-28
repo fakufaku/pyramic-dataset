5ee1a36ab6f6649eb0c04dd3d80908a6f8328aa5
Robin Scheibler <fakufaku@gmail.com>
2018-03-29 00:00:16 +0900

    Added calibrated locations and updated the calibration script to save them

--- code/run_calibration.py
+++ code/run_calibration.py
@@ -10,11 +10,15 @@ import numpy as np
 import json, os, argparse, sys
 from scipy.io import wavfile
 import pyroomacoustics as pra
-import matplotlib.pyplot as plt
 
 sys.path.append('./code')
 
 from calibration import cart2sph, joint_calibration_gd, joint_calibration_sgd, structure_from_sound
+
+# These are the valid angles and speakers
+angles = list(range(0, 360, 2))
+spkr_list = {'middle':0, 'low':1, 'high':2}
+spkrs = sorted(spkr_list.keys())
 
 if __name__ == '__main__':
 
@@ -25,6 +29,10 @@ if __name__ == '__main__':
             help='The file containing the TDOA measurements')
     parser.add_argument('-m', '--method', type=str, choices=methods,
             help='The calibration method to use ''gd'' or ''svd''')
+    parser.add_argument('-s', '--save', type=str,
+            help='A JSON file where to save the callibrated microphones and sources locations')
+    parser.add_argument('-p', '--plot', action='store_true',
+            help='Shows the callibrated locations in a plot')
     args = parser.parse_args()
 
     with open(args.file, 'r') as f:
@@ -101,61 +109,91 @@ if __name__ == '__main__':
     mic_array_cal -= mic_array_cal.mean(axis=1)[:,np.newaxis]
     mic_array -= mic_array.mean(axis=1)[:,np.newaxis]
 
+    # Save the calibrated locations in a file
+    if args.save is not None:
 
-    # Plot stuff
-    plt.figure()
-    plt.semilogy(convergence_curve)
-    plt.title('Convergence')
+        # transform to spherical coordinates and reshape
+        gain, col, az = cart2sph(*P)
+        col = col.reshape(shape)
+        az = az.reshape(shape)
+        gain = gain.reshape(shape)
 
-    sources = pra.experimental.PointCloud(X=P0)
-    sources_cal = pra.experimental.PointCloud(X=P)
+        # build a structure with the calibration information
+        calibration_info = dict()
+        calibration_info['speakers_numbering'] = spkr_list
+        calibration_info['microphones'] = X.T.tolist()
 
-    mics = pra.experimental.PointCloud(X=mic_array)
-    mics_cal = pra.experimental.PointCloud(X=mic_array_cal)
+        calibration_info['sources'] = {}
+        for i_s, spkr in enumerate(spkrs):
+            calibration_info['sources'][spkr] = {
+                    'gain' : dict(zip(angles, gain[:,i_s])),
+                    'azimuth' : dict(zip(angles, az[:,i_s])),
+                    'colatitude' : dict(zip(angles, col[:,i_s])),
+                    }
 
-    ax = mics.plot()
-    mics_cal.plot(show_labels=False, axes=ax, c='r')
-    ax.set_aspect('equal')
-
-    max_range = 0.5 * (X.max() - X.min())
-    mid = (P.max(axis=1) + P.min(axis=1)) * 0.5
-    ax.set_xlim(mid[0] - max_range, mid[0] + max_range)
-    ax.set_ylim(mid[1] - max_range, mid[1] + max_range)
-    ax.set_zlim(mid[2] - max_range, mid[2] + max_range)
-
-    ax2 = sources.plot(show_labels=False)
-    sources_cal.plot(show_labels=False, axes=ax2, c='r')
-    ax2.set_aspect('equal')
-    ax.set_aspect('equal')
-
-    max_range = 0.5 * (P.max() - P.min())
-    mid = (P.max(axis=1) + P.min(axis=1)) * 0.5
-    ax2.set_xlim(mid[0] - max_range, mid[0] + max_range)
-    ax2.set_ylim(mid[1] - max_range, mid[1] + max_range)
-    ax2.set_zlim(mid[2] - max_range, mid[2] + max_range)
-
-    plt.figure()
-
-    r0, col0, az0 = cart2sph(*P0)
-    I = np.argsort(az0[::3])
-    col0 = col0.reshape(shape)[I,:]
-    az0 = az0.reshape(shape)[I,:]
-    r, col, az = cart2sph(*P)
-    col = col.reshape(shape)[I,:]
-    az = az.reshape(shape)[I,:]
-
-    plt.subplot(1,2,1)
-    plt.plot(np.degrees(az0), np.degrees(col))
-    plt.plot(np.degrees(az0), np.degrees(col0))
-    plt.legend(['high', 'low', 'middle'])
-    plt.xlabel('colatitude set')
-    plt.ylabel('error')
-
-    plt.subplot(1,2,2)
-    plt.plot(np.degrees(az0), np.degrees(az - az0))
-    plt.legend(['high', 'low', 'middle'])
-    plt.xlabel('azimuth set')
-    plt.ylabel('error')
+        with open(args.save, 'w') as f:
+            json.dump(calibration_info, f, indent=2)
 
 
-    plt.show()
+    # Display the calibrated locations
+    if args.plot:
+        import matplotlib.pyplot as plt
+
+        # Plot stuff
+        plt.figure()
+        plt.semilogy(convergence_curve)
+        plt.title('Convergence')
+
+        sources = pra.experimental.PointCloud(X=P0)
+        sources_cal = pra.experimental.PointCloud(X=P)
+
+        mics = pra.experimental.PointCloud(X=mic_array)
+        mics_cal = pra.experimental.PointCloud(X=mic_array_cal)
+
+        ax = mics.plot()
+        mics_cal.plot(show_labels=False, axes=ax, c='r')
+        ax.set_aspect('equal')
+
+        max_range = 0.5 * (X.max() - X.min())
+        mid = (P.max(axis=1) + P.min(axis=1)) * 0.5
+        ax.set_xlim(mid[0] - max_range, mid[0] + max_range)
+        ax.set_ylim(mid[1] - max_range, mid[1] + max_range)
+        ax.set_zlim(mid[2] - max_range, mid[2] + max_range)
+
+        ax2 = sources.plot(show_labels=False)
+        sources_cal.plot(show_labels=False, axes=ax2, c='r')
+        ax2.set_aspect('equal')
+        ax.set_aspect('equal')
+
+        max_range = 0.5 * (P.max() - P.min())
+        mid = (P.max(axis=1) + P.min(axis=1)) * 0.5
+        ax2.set_xlim(mid[0] - max_range, mid[0] + max_range)
+        ax2.set_ylim(mid[1] - max_range, mid[1] + max_range)
+        ax2.set_zlim(mid[2] - max_range, mid[2] + max_range)
+
+        plt.figure()
+
+        # transform to spherical coordinates and reshape
+        gain0, col0, az0 = cart2sph(*P0)
+        I = np.argsort(az0[::3])
+        col0 = col0.reshape(shape)[I,:]
+        az0 = az0.reshape(shape)[I,:]
+        gain, col, az = cart2sph(*P)
+        col = col.reshape(shape)[I,:]
+        az = az.reshape(shape)[I,:]
+
+        plt.subplot(1,2,1)
+        plt.plot(np.degrees(az0), np.degrees(col))
+        plt.plot(np.degrees(az0), np.degrees(col0))
+        plt.legend(['high', 'low', 'middle'])
+        plt.xlabel('colatitude set')
+        plt.ylabel('error')
+
+        plt.subplot(1,2,2)
+        plt.plot(np.degrees(az0), np.degrees(az - az0))
+        plt.legend(['high', 'low', 'middle'])
+        plt.xlabel('azimuth set')
+        plt.ylabel('error')
+
+
+        plt.show()
