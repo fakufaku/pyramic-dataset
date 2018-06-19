94247d1f4ec0e5b9f5ca250c1ebc7b3520d69d2d
Robin Scheibler <fakufaku@gmail.com>
2018-06-19 23:02:41 +0900

    Corrected source numbers in the download script

--- download_segmented.sh
+++ download_segmented.sh
@@ -2,10 +2,10 @@
 
 # This will download all the segmented samples
 wget -qO- https://zenodo.org/record/1209563/files/pyramic_segmented_fq_sample0.tar.gz | tar xzv
-wget -qO- https://zenodo.org/record/1209563/files/pyramic_segmented_fq_sample0.tar.gz | tar xzv
-wget -qO- https://zenodo.org/record/1209563/files/pyramic_segmented_fq_sample0.tar.gz | tar xzv
-wget -qO- https://zenodo.org/record/1209563/files/pyramic_segmented_fq_sample0.tar.gz | tar xzv
-wget -qO- https://zenodo.org/record/1209563/files/pyramic_segmented_fq_sample0.tar.gz | tar xzv
+wget -qO- https://zenodo.org/record/1209563/files/pyramic_segmented_fq_sample1.tar.gz | tar xzv
+wget -qO- https://zenodo.org/record/1209563/files/pyramic_segmented_fq_sample2.tar.gz | tar xzv
+wget -qO- https://zenodo.org/record/1209563/files/pyramic_segmented_fq_sample3.tar.gz | tar xzv
+wget -qO- https://zenodo.org/record/1209563/files/pyramic_segmented_fq_sample4.tar.gz | tar xzv
 wget -qO- https://zenodo.org/record/1209563/files/pyramic_segmented_silence.tar.gz | tar xzv
 wget -qO- https://zenodo.org/record/1209563/files/pyramic_segmented_sweep_exp.tar.gz | tar xzv
 wget -qO- https://zenodo.org/record/1209563/files/pyramic_segmented_sweep_lin.tar.gz | tar xzv
